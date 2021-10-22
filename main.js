5bb9f7b9a3a80d57ec707793382322125365f9f5
cemanacajunio12345@gmail.com <cemanacajunio12345@gmail.com>
2021-10-21 22:18:46 -0500

    codigo

---
+++ main.js
@@ -0,0 +1,47 @@
+let nombre = 'cesar mauricio navas calderon';
+console.log(nombre);
+console.log(Object(nombre));
+
+// Dato Number
+let edad = 15;
+console.log(edad);
+console.log(Object(edad));
+
+// Dato Number
+let altura = 1.73;
+console.log(altura);
+console.log(Object(altura));
+
+// Dato boolean
+let soyestudiante = true;
+console.log(soyestudiante);
+console.log(Object(soyestudiante));
+
+// Datos null
+let pelo = null;
+console.log(pelo);
+console.log(Object(pelo));
+
+// Dato undefined
+let brazos; 
+console.log(brazos);
+console.log(Object(brazos));
+
+// Datos Array
+let pasatiempos = ["musica", "aprender", "leer"];
+console.log(pasatiempos);
+
+// Datos Array
+let contacto = [];
+contacto['Celular'] = 3173010912;
+contacto['email'] = "cemanacajunio12345@gmail.com";
+console.log(contacto);
+
+// Datos Objeto
+let dirrecion = {
+    carrera: "7",
+    numero: " 9-F10",
+    departamento: "Santander",
+    municipio:"socorro"
+};
+console.log(dirrecion);
